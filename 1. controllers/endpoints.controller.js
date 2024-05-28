f16b62f497fe5b80a507f64bbed3a58ffcdd3802
JonnyCodeMaster <jon.farmer@hotmail.co.uk>
2024-05-28 16:38:27 +0100

    cleans merge conflicts

--- 1. controllers/endpoints.controller.js
+++ 1. controllers/endpoints.controller.js
@@ -3,7 +3,6 @@ const { selectEndpoints } = require("../2. models/endpoints.model");
 exports.getEndpoints = (req, res, next) => {
   selectEndpoints()
     .then((endpoints) => {
-        console.log();
       res.status(200).send({ endpoints: JSON.parse(endpoints) });
     })
     .catch((err) => {
