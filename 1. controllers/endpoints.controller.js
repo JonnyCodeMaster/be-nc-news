7922be74debed88377110d1f3bc9f4dd70707b72
JonnyCodeMaster <jon.farmer@hotmail.co.uk>
2024-05-28 16:54:13 +0100

    updates code to remove enpoints.controller.js and add fs.readFile into the endpoints.controller.js instead as a response to feedback.

--- 1. controllers/endpoints.controller.js
+++ 1. controllers/endpoints.controller.js
@@ -1,7 +1,7 @@
-const { selectEndpoints } = require("../2. models/endpoints.model");
+const fs = require("fs/promises");
 
 exports.getEndpoints = (req, res, next) => {
-  selectEndpoints()
+    return fs.readFile("endpoints.json", "utf8")
     .then((endpoints) => {
       res.status(200).send({ endpoints: JSON.parse(endpoints) });
     })
--- 2. models/endpoints.model.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs/promises");
-
-exports.selectEndpoints = () => {
-  return fs.readFile("endpoints.json", "utf8");
-};
