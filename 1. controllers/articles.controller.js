728f8272c70eadc3ee26fba69856c3c23baa6e11
JonnyCodeMaster <jon.farmer@hotmail.co.uk>
2024-05-29 14:22:31 +0100

    Updates app.js, articles.controller.js, articles.model.js and app.test.js impplementing recommended feedback changes.

--- 1. controllers/articles.controller.js
+++ 1. controllers/articles.controller.js
@@ -1,18 +1,10 @@
 const { selectArticles } = require("../2. models/articles.model");
 
 exports.getArticles = (req, res, next) => {
-  const { article_id } = req.query;
-
-  if (!article_id || isNaN(article_id)) {
-    return res.status(400).send({ msg: "Bad Request" });
-  }
-
+  const { article_id } = req.params;
   selectArticles(article_id)
-    .then((articles) => {
-      if (articles.length === 0) {
-        return res.status(404).send({ msg: "Resource Not Found" });
-      }
-      res.status(200).send({ article: articles[0] });
+    .then((article) => {
+      res.status(200).send({ article: article[0] });
     })
     .catch((err) => {
       next(err);
--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const app = express();
 
 app.get("/api/topics", getTopics);
 app.get("/api", getEndpoints);
-app.get("/api/articles/", getArticles);
+app.get("/api/articles/:article_id", getArticles);
 
 app.all("*", (req, res, next) => {
   res.status(404).send({ msg: "Resource Not Found" });
 });
 
 app.use((err, req, res, next) => {
-  if (err.code === "22P02") {
+  if (err.code === "22P02" || err.code === "23502") {
     res.status(400).send({ msg: "Bad Request" });
   } else if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
