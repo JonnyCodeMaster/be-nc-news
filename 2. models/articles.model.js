32874f694a1cc50789b2e46e0d013dcaf3fcc266
JonnyCodeMaster <jon.farmer@hotmail.co.uk>
2024-05-29 16:34:56 +0100

    Creates /api/articles endpont which retrieves all articles sorted by date by descending order.

--- 2. models/articles.model.js
+++ 2. models/articles.model.js
@@ -1,6 +1,6 @@
 const db = require("../db/connection");
 
-exports.selectArticles = (article_id) => {
+exports.selectArticleId = (article_id) => {
   let sqlQuery =
     "SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url FROM articles WHERE articles.article_id = $1;";
   const queryValues = [article_id];
@@ -11,4 +11,34 @@ exports.selectArticles = (article_id) => {
     }
     return rows;
   });
-}; 
+};
+
+exports.selectAllArticles = () => {
+  const sqlQuery = `
+    SELECT 
+      articles.author, 
+      articles.title, 
+      articles.article_id, 
+      articles.topic, 
+      articles.created_at, 
+      articles.votes, 
+      articles.article_img_url, 
+      COUNT(comments.comment_id) AS comment_count 
+    FROM 
+      articles 
+    LEFT JOIN 
+      comments 
+    ON 
+      articles.article_id = comments.article_id 
+    GROUP BY 
+      articles.article_id 
+    ORDER BY 
+      articles.created_at DESC;`;
+
+  return db.query(sqlQuery).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "Resource Not Found" });
+    }
+    return rows;
+  });
+};
