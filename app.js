7c199fa9c5f40292ff9a505f9f8fe4d44d8792ae
JonnyCodeMaster <jon.farmer@hotmail.co.uk>
2024-05-30 12:42:20 +0100

    Creates /api/articles/:article_id/comments endpoint which posts a new comment to a specific article_id.

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const {
   getArticles,
   getArticlesByArticleId,
   getCommentsByArticleId,
+  postCommentByArticleId,
 } = require("./1. controllers/articles.controller");
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/api", getEndpoints);
 app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticlesByArticleId);
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 
 app.all("*", (req, res, next) => {
   res.status(404).send({ msg: "Resource Not Found" });
@@ -22,6 +26,8 @@ app.all("*", (req, res, next) => {
 app.use((err, req, res, next) => {
   if (err.code === "22P02" || err.code === "23502") {
     res.status(400).send({ msg: "Bad Request" });
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "Resource Not Found" });
   } else if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
   } else {
